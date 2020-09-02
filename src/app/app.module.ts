62ff44dd44bc3a6042693eb97d1ab8cad53c700f
Kendy <Benoldk@gmail.com>
2020-09-02 04:01:03 -0400

    added feature to add items to list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { ItemslistService } from './itemslist.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [ItemslistService],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 import { ItemslistService } from '../itemslist.service';
+
 
 @Component({
   selector: 'app-home',
@@ -8,14 +10,35 @@ import { ItemslistService } from '../itemslist.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  itemsList = []
+  itemsList = [];
+  itemText: string = "Add item...";
+  btnText: String = "Add";
 
-  constructor(private _items:ItemslistService) { 
+  constructor(private _items:ItemslistService, private _router:Router) { 
 
   }
 
   ngOnInit(): void {
-    this._items.item.subscribe(itemsList => this.itemsList = itemsList);
+    this._items.item.subscribe(il => this.itemsList = il);
     this._items.changeItem(this.itemsList);
+  }
+
+  addItem() {
+    if(this.itemText !== "") {
+      this.itemsList.push(this.itemText);
+      this.itemText = "";
+      this._items.changeItem(this.itemsList);
+    }
+  }
+
+  redirectToListPage() {
+    this._router.navigate(['./list']);
+  }
+
+  removeItem(index) {
+    if(index >= 0 && index < this.itemsList.length) {
+      this.itemsList.splice(index, 1);
+      this._items.changeItem(this.itemsList);
+    }
   }
 }
